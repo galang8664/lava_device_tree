48d66c793339a2f47b9493131583cadf4a859969
68orkun <hesaphesapyus@gmail.com>
2022-10-03 17:11:34 +0300

    lava: PixelOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/arrow_lava.mk
+   $(LOCAL_DIR)/aosp_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lava-user \
-    arrow_lava-userdebug \
-    arrow_lava-eng
+    aosp_lava-user \
+    aosp_lava-userdebug \
+    aosp_lava-eng
